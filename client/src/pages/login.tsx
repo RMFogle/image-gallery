43247aa39e68463ce6b6407eb62cc1e1a5bddb83
RyFogle <13fogle@gmail.com>
2021-10-22 15:08:42 -0700

    adding login and register

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,8 +1,33 @@
+import { Link } from 'react-router-dom'
+
+import LoginPass from '../components/auth/LoginPass'
 
 const Login = () => {
+
+
     return (
-        <div>
-            <h2>Login Page</h2>
+        <div className="auth_page">
+            <div className="auth_box">
+                <h3 className="text-uppercase text-center mb-4">Login</h3>
+
+                <LoginPass />
+
+                <small className="row my-2 text-primary" style={{cursor: 'pointer'}}>
+                    <span className="col-6">
+                        <Link to="/forgot_password">
+                            Forgot password?
+                        </Link>
+                    </span>
+                </small>
+
+                <p>
+                    You don't have an account? 
+                    <Link to={`/register`} style={{color: 'crimson'}}>
+                        Register Now
+                    </Link>
+                </p>
+
+            </div>
         </div>
     )
 }
---
+++ client/src/redux/actions/authAction.ts
@@ -0,0 +1,45 @@
+import { Dispatch } from "redux";
+import { AUTH, IAuthType } from '../types/authType'
+import { ALERT, IAlertType } from '../types/alertType'
+
+import { IUserLogin, IUserRegister } from "../../utils/TypeScript";
+import { postAPI } from "../../utils/Api";
+import { validRegister } from "../../utils/Valid";
+
+export const login = (userLogin: IUserLogin) => 
+    async (dispatch: Dispatch<IAuthType | IAlertType>) => {
+        try {
+            dispatch({ type: ALERT, payload: { loading: true } })
+            const res: any = await postAPI('login', userLogin);
+
+            dispatch({
+                type: AUTH,
+                payload: {
+                    token: res.data.access_token,
+                    user: res.data.user
+                }
+            });
+
+            dispatch({ type: ALERT, payload: { success: res.data.msg } })
+        } catch (err: any) {
+            dispatch({ type: ALERT, payload: { errors: err.response.data.msg }})
+        }
+    }
+
+export const register = (userRegister: IUserRegister) => 
+    async (dispatch: Dispatch<IAuthType | IAlertType>) => {
+        const check = validRegister(userRegister)
+
+        if(check.errLength > 0)
+            return dispatch({ type: ALERT, payload: { errors: check.errMsg } })
+        
+        try {
+            dispatch({ type: ALERT, payload: { loading: true } })
+
+            const res: any = await postAPI('register', userRegister)
+
+            dispatch({ type: ALERT, payload: { success: res.data.msg } })
+        } catch (err: any) {
+            dispatch({ type: ALERT, payload: { errors: err.response.data.msg } })
+        }
+    }
